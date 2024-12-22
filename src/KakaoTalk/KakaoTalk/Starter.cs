b9d6a0f0772502c0cdb674f523ee424ee3b25053
rsj9987 <rsj9987@gmail.com>
2024-12-22 15:43:36 +0900

    Add Starter

---
+++ src/KakaoTalk/KakaoTalk/Starter.cs
@@ -0,0 +1,11 @@
+﻿namespace KakaoTalk
+{
+    internal class Starter
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            _ = new App().Run();
+        }
+    }
+}
