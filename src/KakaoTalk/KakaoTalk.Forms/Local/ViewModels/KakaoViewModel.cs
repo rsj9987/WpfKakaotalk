e846ae58a7a76221704eacfc019f38303b355ff9
rsj9987 <rsj9987@gmail.com>
2024-12-22 20:27:54 +0900

    Add comment at KakaoViewModel.cs

--- src/KakaoTalk/KakaoTalk.Forms/Local/ViewModels/KakaoViewModel.cs
+++ src/KakaoTalk/KakaoTalk.Forms/Local/ViewModels/KakaoViewModel.cs
@@ -25,6 +25,8 @@ namespace KakaoTalk.Forms.Local.ViewModels
 
         public void OnLoaded(IViewable view)
         {
+            // CustomControl 생성자가 여러번 Resolve 함수를 탈 때마다 생성되는 것을 방지하기 위해 처음 Load 될 때 등록해놓는다.
+            // KakakTalk -> DirectModules.cs 참조
             IRegion mainRegion = _regionManager.Regions["MainRegion"];
 
             var loginContent = _containerProvider.Resolve<IViewable>("LoginContent");
