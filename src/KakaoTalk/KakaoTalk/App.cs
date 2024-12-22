c5cd12d7ca3830f88dc1ac22e0fd5f92acdef5bb
rsj9987 <rsj9987@gmail.com>
2024-12-22 17:07:14 +0900

    Add Window

--- src/KakaoTalk/KakaoTalk/App.cs
+++ src/KakaoTalk/KakaoTalk/App.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 
+using KakaoTalk.Forms.UI.Views;
+
 namespace KakaoTalk
 {
     internal class App : Application
@@ -8,7 +10,7 @@ namespace KakaoTalk
         {
             base.OnStartup(e);
 
-            Window window = new Window();
+            KakaoWindow window = new();
             window.Show();
         }
     }
--- src/KakaoTalk/KakaoTalk/obj/Debug/net8.0-windows/KakaoTalk.AssemblyInfo.cs
+++ src/KakaoTalk/KakaoTalk/obj/Debug/net8.0-windows/KakaoTalk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KakaoTalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d6a0f0772502c0cdb674f523ee424ee3b25053")]
 [assembly: System.Reflection.AssemblyProductAttribute("KakaoTalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KakaoTalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
