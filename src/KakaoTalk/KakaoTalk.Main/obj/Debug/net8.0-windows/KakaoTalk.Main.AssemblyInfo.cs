74b2ca0db6faa68a1d39920985a087e29df5e3d1
rsj9987 <rsj9987@gmail.com>
2024-12-23 10:30:09 +0900

    Delete Chat

--- src/KakaoTalk/KakaoTalk.Main/obj/Debug/net8.0-windows/KakaoTalk.Main.AssemblyInfo.cs
+++ src/KakaoTalk/KakaoTalk.Main/obj/Debug/net8.0-windows/KakaoTalk.Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KakaoTalk.Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7312c885c2cdf651f1def67e0369ffc902c4cece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f143823cf7e89f258afacd856e0192feee8ec6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("KakaoTalk.Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KakaoTalk.Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
